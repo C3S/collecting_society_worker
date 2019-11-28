79f1fe465962a1888903b74bf8d13f8832c7e0b1
Thomas Mielke <thomas.mielke@c3s.cc>
2019-11-28 17:26:19 +0100

    some integration testing of collecting_society_worker with EchoPrint server

--- collecting_society_worker/tests/unit/test_something.py
+++ collecting_society_worker/tests/unit/test_something.py
@@ -3,9 +3,10 @@ from collecting_society_worker import fileTools
 
 class TestSomething(unittest.TestCase):
 
-    def test_something(self):
-        """
-        something
-        """
-        fileTools.get_filename("a/b")
-        assert(True)
+    # def test_something(self):
+    #     """
+    #     something
+    #     """
+    #     fileTools.get_filename("a/b")
+    #     assert(True)
+    pass
