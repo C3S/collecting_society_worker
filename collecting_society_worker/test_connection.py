437b5047f16198b174acffbbec97cee4b8e95a5a
Alexander Blum <alexander.blum@c3s.cc>
2021-03-26 23:00:24 +0100

    adds connection test script

---
+++ collecting_society_worker/test_connection.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# For copyright and license terms, see COPYRIGHT.rst (top level of repository)
+# Repository: https://github.com/C3S/collecting_society_worker
+
+"""
+Script to test the proteus connection
+"""
+
+from repro import connect_db
+
+connect_db()
